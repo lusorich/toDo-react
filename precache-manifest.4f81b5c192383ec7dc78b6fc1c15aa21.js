98d8886b0d960a5aaff1e49f5f515b1e5496ad52
lusorich <dreamonautre@gmail.com>
2021-01-23 02:50:28 +0500

    Updates

---
+++ precache-manifest.4f81b5c192383ec7dc78b6fc1c15aa21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96f673a7fe756b719d5d0fce7a2760c",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "631f78766ad1991008aa",
+    "url": "/todo-react/static/css/main.8d6479d2.chunk.css"
+  },
+  {
+    "revision": "05179f275fdea949f9c2",
+    "url": "/todo-react/static/js/2.84f7c15e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react/static/js/2.84f7c15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631f78766ad1991008aa",
+    "url": "/todo-react/static/js/main.7c4e725f.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);
